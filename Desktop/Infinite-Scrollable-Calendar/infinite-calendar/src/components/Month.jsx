1cfb579716fd9cea249d5e8b6b214ba9fccbb82d
Nikhil Gupta <164629304+Nikhil112004@users.noreply.github.com>
2025-09-16 01:51:56 +0530

    Update Month.jsx

--- Desktop/Infinite-Scrollable-Calendar/infinite-calendar/src/components/Month.jsx
+++ Desktop/Infinite-Scrollable-Calendar/infinite-calendar/src/components/Month.jsx
@@ -7,12 +7,10 @@ function Month({ month, year }) {
 
   return (
     <div className="p-4 border rounded-xl shadow-sm bg-white">
-      {/* Month Title */}
       <h2 className="text-lg font-bold mb-2 text-center">
         {format(start, "MMMM yyyy")}
       </h2>
 
-      {/* Days Grid */}
       <div className="grid grid-cols-7 gap-2 text-center">
         {["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"].map((d) => (
           <div key={d} className="text-sm font-semibold text-gray-500">
